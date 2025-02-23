8624923ff9884ea9da79246d1824d715e17b0c72
Aniket Ghosh <ghoshaniket1000@gmail.com>
2025-02-23 13:18:37 +0530

    created helper functions for json

--- broker-service/cmd/api/handler.go
+++ broker-service/cmd/api/handler.go
@@ -1,15 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
-
-type jsonResponse struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Data    any    `json:"data,omitempty"`
-}
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 
@@ -18,9 +11,5 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 		Message: "Hit the Broker",
 	}
 
-	out, _ := json.MarshalIndent(payload, "", "\t")
-
-	w.Header().Set("Content-Type", "Application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write(out)
+	_ = app.writeJSON(w, http.StatusOK, payload)
 }
